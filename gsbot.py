eaff9a402f2031569e714dcbef3dff3db2a4c9b9
Pachev Joseph <pachevjoseph@gmail.com>
2017-06-03 21:34:42 -0400

    Adds some more commands to bot

--- gsbot.py
+++ gsbot.py
@@ -3,6 +3,7 @@ from sqlalchemy.orm import *
 from sqlalchemy.ext.declarative import declarative_base
 from configparser import ConfigParser  
 from tabulate import tabulate
+import numpy as np
 
 
 import asyncio
@@ -13,7 +14,7 @@ import random
 
 #Connecting to the sqlite database that holds the user information
 #Note: The database table has already been populated
-engine = create_engine('sqlite:///gsbot.db', echo=True)
+engine = create_engine('sqlite:///gsbot.db', echo=False)
 
 Base = declarative_base()
 Session = sessionmaker(bind=engine)
@@ -39,7 +40,6 @@ class Member(Base):
     dp = Column(Integer)
     gear_score = Column(Integer)
 
-members = session.query(Member).order_by(Member.gear_score.desc()).all()
 
 description = '''
 This the official Gear Score bot of the Legendary Guild Sazerac.
@@ -71,30 +71,71 @@ async def list(num=10):
     """List all the members and their gear score with optional limit. 
     eg. ?list returns first 10 by default  and ?list 5 first 5 sorted by
     gear score"""
-
-    data =tabulate([[u.fam_name,u.char_name, u.gear_score] for u in members[:num]], 
+    members = session.query(Member).order_by(Member.gear_score.desc()).all()
+    if num > 45:
+        nums = 45
+    else:
+        nums = num
+    data =tabulate([[u.fam_name,u.char_name, u.gear_score] for u in members[:nums]], 
                    ['Family', 'Character', 'GearScore'],
                    'simple',
                    stralign='center')
     await bot.say(data)
 
+@bot.command()
+async def over(num=350):
+    """List all the members over a certain gear score"""
+    members = session.query(Member).order_by(Member.gear_score.desc()).all()
+    data =tabulate([[u.fam_name,u.char_name, u.gear_score] for u in members[1:40] if u.gear_score > num], 
+                   ['Family', 'Character', 'GearScore'],
+                   'simple',
+                   stralign='center')
+    print(data)
+    await bot.say(data)
 
-@bot.group(pass_context=True)
-async def cool(ctx):
-    """Says if a user is cool.
-    In reality this just checks if a subcommand is being invoked.
-    """
-    if ctx.invoked_subcommand is None:
-        await bot.say('No, {0.subcommand_passed} is not cool'.format(ctx))
+@bot.command()
+async def lookup(name=""):
+    """Looks up a guild member by their family name for score"""
 
-@cool.command(name='bot')
-async def _bot():
-    """Is the bot cool?"""
-    await bot.say('Yes, the bot is cool.')
 
-@cool.command(name='drawven')
-async def _bot():
-    """Is the bot cool?"""
-    await bot.say('Yep, he is cool.')
+    member = session.query(Member).filter(Member.fam_name.ilike('%'+name+'%')).all()
+    data =tabulate([[u.fam_name,u.char_name, u.gear_score] for u in member], 
+                   ['Family', 'Character', 'GearScore'],
+                   'simple',
+                   stralign='center')
+    print(data)
+    await bot.say(data)
+
+@bot.command()
+async def average():
+    """Gives the current guild score average"""
+
+    members = session.query(Member).order_by(Member.gear_score.desc()).all()
+    gear_scores = [u.gear_score for u in members[1:]]
+    average = np.average(gear_scores)
+    print(average)
+    await bot.say(average)
+
+@bot.command()
+async def info():
+    """List important gear score info. Lowest, highest, average, newest(not yet), oldest, total users, total officers, total members"""
+
+    info = []
+    members = session.query(Member).order_by(Member.gear_score.desc()).all()
+    officers = session.query(Member).filter(Member.rank == 'officer').count()
+    memb = session.query(Member).filter(Member.rank == 'member').count()
+    gear_scores = [u.gear_score for u in members[ 1: ]]
+    average = np.average(gear_scores)
+    lowest = members[-1]
+    highest = members[1]
+    info.append(['Average', round(average,2)])
+    info.append(['Lowest', lowest.gear_score, lowest.fam_name, lowest.char_name])
+    info.append(['Higest', highest.gear_score, highest.fam_name, highest.char_name])
+    info.append(['Total Officers', officers])
+    info.append(['Total Members', memb])
+    info.append(['Total', len(members)])
+
+    await bot.say(tabulate(info))
+
 
 bot.run(token)
