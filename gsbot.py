b5f25845aadc79b1c2db34dd70e7aada86040610
Pachev Joseph <pachevjoseph@gmail.com>
2017-06-05 10:19:59 -0400

    Adds officer only commands add, update, and delete

--- gsbot.py
+++ gsbot.py
@@ -4,6 +4,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from configparser import ConfigParser  
 from tabulate import tabulate
 import numpy as np
+from datetime import datetime
 
 
 import asyncio
@@ -22,15 +23,16 @@ session = Session()
 
 config = ConfigParser()  
 config.read('config.ini')  
-token = config.get('auth', 'token') 
+token = config.get('auth', 'token2') 
 
 #this is where I declare the class for the ORM based on the original spreadsheet
 class Member(Base):
-    __tablename__ = 'score_sheet'
+    __tablename__ = 'gs_bot'
     __table_args__ = {'sqlite_autoincrement': True}
     id = Column(Integer, primary_key=True, nullable=False)
     fam_name = Column(String)
     char_name = Column(String)
+    discord = Column(String)
     notes = Column(String)
     updated = Column(String)
     char_class = Column(String)
@@ -45,7 +47,7 @@ description = '''
 This the official Gear Score bot of the Legendary Guild Sazerac.
 Made by drawven(drawven@gmail.com)
 '''
-bot = commands.Bot(command_prefix='?', description=description)
+bot = commands.Bot(command_prefix='gsbot ', description=description)
 
 
 @bot.event
@@ -55,16 +57,192 @@ async def on_ready():
     print(bot.user.id)
     print('------')
 
-@bot.event
-async def on_message(message):
-    if message.content.startswith('gsbot add'):
-        await bot.send_message(message.channel,"Addin player")
-        print(message.author)
-    elif message.content.startswith('gsbot sleep'):
-        await asyncio.sleep(5)
-        await bot.send_message(message.channel, 'Done sleeping for 5')
+@bot.command(pass_context=True)
+async def add_me(ctx, fam_name, char_name, level: int, ap : int, dp: int, char_class):
+    """Adds yourself as a memberto the database. This member is linked with your discord name
+    and can only be updated by either that member or an officer. The command is as
+    example regular member: gsbot add drawven drawven 56 83 83 Musa 
+    Note: Total gear score is auto calculated"""
+    date = datetime.now()
 
-    await bot.process_commands(message)
+    author = ctx.message.author
+    count = session.query(Member).filter(Member.discord == ctx.message.author.id).count()
+
+    roles = [u.name for u in author.roles]
+
+    if 'Admin' in roles:
+        rank = 'Officer'
+    else:
+        rank = 'Member'
+
+    if count >= 1:
+        await bot.say("Cannot add more than one character to this discord id")
+    else:
+        try:
+            member = Member(fam_name=fam_name, 
+                            char_name=char_name, 
+                            level= level,
+                            ap = ap,
+                            dp = dp,
+                            char_class= char_class,
+                            rank = rank,
+                            gear_score = ap + dp,
+                            discord = ctx.message.author.id,
+                            notes = "",
+                            updated =  date.strftime("%D"))
+            session.add(member)
+            session.commit()
+            info = [["Success Adding User"], ["Character", char_name], ["gear_score", ap+dp], ["Discord", ctx.message.author.id]]
+
+            await bot.say(tabulate(info))
+            
+        except:
+            await bot.say("Something went horribly wrong")
+
+@bot.command(pass_context=True)
+async def add(ctx, 
+              fam_name,
+              char_name,
+              level: int,
+              ap : int,
+              dp: int,
+              char_class,
+              user: discord.Member,
+              rank="Member"):
+    """Officers only Adds a member to the database. 
+    example : gsbot add drawven drawven 56 83 83 Musa @drawven#9405
+    Note: Total gear score is auto calculated"""
+    date = datetime.now()
+
+    author = ctx.message.author
+    count = session.query(Member).filter(Member.discord == user.id).count()
+
+    roles = [u.name for u in author.roles]
+    print([i.name for i in author.roles])
+    if 'Admin' not in roles:
+        await bot.say("Only officers may perorm this action")
+    else:
+        if count >= 1:
+            await bot.say("Cannot add more than one character to this discord id")
+        else:
+            try:
+                member = Member(fam_name=fam_name, 
+                                char_name=char_name, 
+                                level= level,
+                                ap = ap,
+                                dp = dp,
+                                char_class= char_class,
+                                rank = rank,
+                                gear_score = ap + dp,
+                                discord = user.id,
+                                notes = "",
+                                updated =  date.strftime("%D"))
+                session.add(member)
+                session.commit()
+                info = [["Success Adding User"], ["Character", char_name], ["gear_score", ap+dp], ["Discord", user.id]]
+
+                await bot.say(tabulate(info))
+                
+            except:
+                await bot.say("Something went horribly wrong")
+
+
+@bot.command(pass_context=True)
+async def update_me(ctx, level: int, ap: int, dp: int):
+    """Updates a users gear score Each user is linked to a gear score in the database
+    and can only update their scores(excluding officers)."""
+    date = datetime.now()
+
+    try:
+        member = session.query(Member).filter(Member.discord == ctx.message.author.id).one()
+        member.level = level
+        member.ap = ap
+        member.dp = dp
+        member.gear_score = ap + dp
+        member.updated = date.strftime("%D")
+
+        session.add(member)
+        info = [["Success updating User"], ["Character", member.char_name], ["gear_score", ap+dp], ["Discord", ctx.message.author.id]]
+
+        session.commit()
+
+        await bot.say(tabulate(info))
+        
+    except Error:
+        print(Error)
+        await bot.say("Error updating user")
+
+@bot.command(pass_context=True)
+async def update(ctx, level: int, ap: int, dp: int, user: discord.Member):
+    """ Officers Only Updates a users gear score Each user is linked to a gear score in the database
+    and can only update their scores(excluding officers)."""
+
+    author = ctx.message.author
+
+    roles = [u.name for u in author.roles]
+    print([i.name for i in author.roles])
+    if 'Admin' not in roles:
+        await bot.say("Only officers may perorm this action")
+    else:
+        date = datetime.now()
+
+        try:
+            member = session.query(Member).filter(Member.discord == user.id).one()
+            member.level = level
+            member.ap = ap
+            member.dp = dp
+            member.gear_score = ap + dp
+            member.updated = date.strftime("%D")
+
+            session.add(member)
+            info = [["Success updating User"], ["Character", member.char_name], ["gear_score", ap+dp], ["Discord", ctx.message.author.id]]
+
+            session.commit()
+
+            await bot.say(tabulate(info))
+            
+        except Error:
+            print(Error)
+            await bot.say("Error updating user")
+
+@bot.command(pass_context=True)
+async def delete(ctx, user: discord.Member):
+    """Officers only Deletes an added character"""
+
+    author = ctx.message.author
+    count = session.query(Member).filter(Member.discord == user.id).count()
+
+    roles = [u.name for u in author.roles]
+    print([i.name for i in author.roles])
+    if 'Admin' not in roles:
+        await bot.say("Only officers may perorm this action")
+    else:
+        try:
+            member = session.query(Member).filter(Member.discord == user.id).one()
+            session.delete(member)
+            info = [["Success Deleting User"], ["Character", member.char_name], ["Discord", user.id]]
+            session.commit()
+
+            await bot.say(tabulate(info))
+            
+        except:
+            await bot.say("Error deleting user")
+
+@bot.command(pass_context=True)
+async def delete_me(ctx):
+    """Deletes your added character"""
+
+    try:
+        member = session.query(Member).filter(Member.discord == ctx.message.author.id).one()
+        session.delete(member)
+        info = [["Success Deleting User"], ["Character", member.char_name], ["Discord", ctx.message.author.id]]
+        session.commit()
+
+        await bot.say(tabulate(info))
+        
+    except:
+        await bot.say("Error deleting user")
+
 
 @bot.command()
 async def list(num=10):
@@ -76,17 +254,28 @@ async def list(num=10):
         nums = 45
     else:
         nums = num
-    data =tabulate([[u.fam_name,u.char_name, u.gear_score] for u in members[:nums]], 
-                   ['Family', 'Character', 'GearScore'],
+    data =tabulate([[u.fam_name,u.char_name, u.rank, u.char_class, u.level, u.ap, u.dp, u.gear_score, u.updated] for u in members[:nums]], 
+                   ['Family', 'Character', 'Rank', 'Class', 'Level', 'Ap', 'DP','GearScore', 'Updated'],
                    'simple',
-                   stralign='center')
+                   stralign='left')
     await bot.say(data)
 
 @bot.command()
 async def over(num=350):
     """List all the members over a certain gear score"""
     members = session.query(Member).order_by(Member.gear_score.desc()).all()
-    data =tabulate([[u.fam_name,u.char_name, u.gear_score] for u in members[1:40] if u.gear_score > num], 
+    data =tabulate([[u.fam_name,u.char_name, u.gear_score] for u in members if u.gear_score >= num], 
+                   ['Family', 'Character', 'GearScore'],
+                   'simple',
+                   stralign='center')
+    print(data)
+    await bot.say(data)
+
+@bot.command()
+async def under(num=350):
+    """List all the members under a certain gear score"""
+    members = session.query(Member).order_by(Member.gear_score.desc()).all()
+    data =tabulate([[u.fam_name,u.char_name, u.gear_score] for u in members if u.gear_score <= num], 
                    ['Family', 'Character', 'GearScore'],
                    'simple',
                    stralign='center')
@@ -95,10 +284,9 @@ async def over(num=350):
 
 @bot.command()
 async def lookup(name=""):
-    """Looks up a guild member by their family name for score"""
-
-
-    member = session.query(Member).filter(Member.fam_name.ilike('%'+name+'%')).all()
+    """Looks up a guild member by score"""
+    member = session.query(Member).filter(or_(Member.fam_name.ilike('%'+name+'%'),
+                                              Member.char_name.ilike('%'+name+'%'))).all()
     data =tabulate([[u.fam_name,u.char_name, u.gear_score] for u in member], 
                    ['Family', 'Character', 'GearScore'],
                    'simple',
@@ -111,7 +299,7 @@ async def average():
     """Gives the current guild score average"""
 
     members = session.query(Member).order_by(Member.gear_score.desc()).all()
-    gear_scores = [u.gear_score for u in members[1:]]
+    gear_scores = [u.gear_score for u in members]
     average = np.average(gear_scores)
     print(average)
     await bot.say(average)
@@ -122,17 +310,16 @@ async def info():
 
     info = []
     members = session.query(Member).order_by(Member.gear_score.desc()).all()
-    officers = session.query(Member).filter(Member.rank == 'officer').count()
-    memb = session.query(Member).filter(Member.rank == 'member').count()
-    gear_scores = [u.gear_score for u in members[ 1: ]]
+    officers = session.query(Member).filter(Member.rank == 'Officer').count()
+    gear_scores = [u.gear_score for u in members]
     average = np.average(gear_scores)
     lowest = members[-1]
-    highest = members[1]
+    highest = members[0]
     info.append(['Average', round(average,2)])
     info.append(['Lowest', lowest.gear_score, lowest.fam_name, lowest.char_name])
     info.append(['Higest', highest.gear_score, highest.fam_name, highest.char_name])
     info.append(['Total Officers', officers])
-    info.append(['Total Members', memb])
+    info.append(['Total Members', len(members) - officers])
     info.append(['Total', len(members)])
 
     await bot.say(tabulate(info))
