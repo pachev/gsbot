a3c9d8ef17158e69c0228853dcdf6e558344b43a
Pachev Joseph <pachevjoseph@gmail.com>
2018-05-10 20:36:25 -0700

    Adds delete_all function for officers

--- cogs/delete.py
+++ cogs/delete.py
@@ -35,6 +35,27 @@ class Delete:
             print_error(e)
             await self.bot.say(codify("Error deleting user"))
 
+    @commands.command(pass_context=True)
+    async def delete_all(self, ctx):
+        """****Officers only***** Deletes All Characters from the server"""
+
+        try:
+            author = ctx.message.author
+            roles = [u.name for u in author.roles]
+            if ADMIN_USER not in roles:
+                await self.bot.say("Only officers may perform this action")
+                return
+            await self.bot.say(codify("Are you sure you want to delete all characters?(times out in 10 secs)\n Reply Yes to confirm"))
+            msg = await self.bot.wait_for_message(author=ctx.message.author, content="Yes", timeout=10)
+            if msg and msg.content == "Yes":
+                Character.objects(server = ctx.message.server.id).delete()
+                await self.bot.say(codify("Success Clearing All Characters on Server"))
+                return
+            await self.bot.say(codify("Operation Cancelled"))
+        except Exception as e:
+            print(e)
+            await self.bot.say(codify("Error deleting all characters"))
+
 
 def setup(bot):
     bot.add_cog(Delete(bot))
